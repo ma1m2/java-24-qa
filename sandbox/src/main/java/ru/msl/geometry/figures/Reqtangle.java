22f5d6ea0b1f230a4d532ab5f3afe204f31fa2b1
other <other@mail.ru>
2024-07-31 22:31:49 +0300

    Record in java 14

--- sandbox/src/main/java/ru/msl/geometry/figures/Reqtangle.java
+++ sandbox/src/main/java/ru/msl/geometry/figures/Reqtangle.java
@@ -1,6 +1,7 @@
 package ru.msl.geometry.figures;
 
-public class Reqtangle {
+public record Reqtangle (double a, double b) {
+
   public static void printRectangleArea(double a, double b) {
     String text = String.format("Area of rectangle with a side %f and a side %f = %f", a, b, rectangleArea(a, b));
     System.out.println(text);
@@ -8,5 +9,12 @@ public class Reqtangle {
 
   private static double rectangleArea(double a, double b) {
     return a * b;
+  }
+
+  public double area() {
+    return a * b;
+  }
+  public double perimeter() {
+    return 2 * (a + b);
   }
 }
