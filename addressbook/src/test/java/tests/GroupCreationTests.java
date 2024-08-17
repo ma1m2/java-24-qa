a9ec05d7e0a2ae676d5565537ab0eb4a88408af3
other <other@mail.ru>
2024-08-17 19:41:01 +0300

    randomString in TestBase

--- addressbook/src/test/java/tests/GroupCreationTests.java
+++ addressbook/src/test/java/tests/GroupCreationTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class GroupCreationTests extends TestBase {
 
   @Test
-  public void canCreateGroup(){
+  public void canCreateGroup() {
     int groupCount = app.group().getCount();
     app.group().createGroup(new GroupData("group name", "group header", "group footer"));
     int newGroupCount = app.group().getCount();
@@ -23,6 +23,18 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void canCreateGroupWithNameOnly() {
     app.group().createGroup(new GroupData().withName("some group"));
+  }
+
+  @Test
+  public void canCreateMultipleGroup() {
+    int n = 5;
+    int groupCount = app.group().getCount();
+    for (int i = 0; i < n; i++) {
+      app.group().createGroup(new GroupData(randomString(i + 1), "group header" + i, "group footer" + i));
+    }
+    int newGroupCount = app.group().getCount();
+    System.out.println(groupCount + " " + newGroupCount);
+    Assertions.assertEquals(groupCount + n, newGroupCount);
   }
 
 }
--- addressbook/src/test/java/tests/TestBase.java
+++ addressbook/src/test/java/tests/TestBase.java
@@ -3,6 +3,8 @@ package tests;
 import manager.AppManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.Random;
+
 public class TestBase {
   protected static AppManager app;
   //protected AppManager app;
@@ -13,6 +15,23 @@ public class TestBase {
       app = new AppManager();
     }
     app.init(System.getProperty("browser", "firefox"));
+  }
+
+  public String randomString(int length) {
+    var rnd = new Random();
+    var result = "";
+    for (int i = 0; i < length; i++) {
+      result = result + (char)('a' + rnd.nextInt(26));
+    }
+    return result;
+  }
+
+  public String randomStringGPT(int length) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      sb.append((char) ('a' + (int) (Math.random() * 26)));
+    }
+    return sb.toString();
   }
 
   /*  @AfterEach
